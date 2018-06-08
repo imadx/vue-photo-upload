ede2cedf200df9c8ba5ddcdf9ffb70a46c899e94
Ishan Madhusanka <ahtimadhusanka@gmail.com>
2018-06-08 11:16:24 +0530

    add .vue extension plugin name

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const PhotoUpload = {
     install: function(Vue, options) {
-        const VuePhotoUpload = require('./Component/PhotoUpload')
+        const VuePhotoUpload = require('./Component/PhotoUpload.vue')
         Vue.component(VuePhotoUpload.name, VuePhotoUpload)
     }
 };
 
-export default PhotoUpload;
+export default PhotoUpload; 
